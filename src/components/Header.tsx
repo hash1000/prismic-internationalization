2ee0a67a414662e8eaad490677966530c138cf5d
hash1000 <hashirmughal1000@gmail.com>
2025-04-10 18:24:45 +0500

    fixes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,35 +18,48 @@ export default async function Header({ lang }: { lang: string }) {
     <div className="relative">
       <div className="flex flex-col lg:flex-row justify-between items-center absolute top-0 w-full z-10">
         {/* Logo */}
-        <div className=" relative self-start h-20 w-36 sm:w-56 sm:h-28 md:w-60 md:h-32 lg:w-64 lg:h-36 xl:w-72 xl:h-40 aspect-[3/2]">
-  <Image
-    src={data.data.logo.url ?? "/fallback-image.jpg"}
-    alt={data.data.logo.alt ?? "Logo"}
-    fill
-    className="object-cover"
-  />
-</div>
-
+        <div className=" relative self-start h-20 w-auto sm:h-28 md:w-60 md:h-32  xl:h-40 aspect-[3/2]">
+          <Image
+            src={data.data.logo.url ?? "/fallback-image.jpg"}
+            alt={data.data.logo.alt ?? "Logo"}
+            fill
+            className="object-cover"
+          />
+        </div>
 
         {/* Navbar */}
-        <div data-aos="fade-up" data-aos-offset="200" className="flex gap-3 sm:gap-4 p-5 justify-center sm:justify-end flex-wrap w-11/12">
+        <div
+          data-aos="fade-up"
+          data-aos-offset="200"
+          className="flex gap-3 sm:gap-4 p-5 justify-center sm:justify-end flex-wrap w-11/12"
+        >
           {data.data.slices[0]?.primary.navbar.map(
-            (item: Simplify<TopNavbarSliceDefaultPrimaryNavbarItem>, index: number) => (
+            (
+              item: Simplify<TopNavbarSliceDefaultPrimaryNavbarItem>,
+              index: number
+            ) => (
               <div key={index} className="relative group">
                 {/* Main Menu Item */}
                 <div className="flex gap-1 items-center">
                   <PrismicNextLink
                     field={item.link} // Only pass `field` to the `PrismicNextLink`
                     className={`${montserrat.className} text-xs sm:text-xl flex items-center gap-1 uppercase font-[600] hover:text-[#6FDCD6] hover:border-b-2 hover:border-[#6FDCD6]`}
-                  /> 
-                  {(item.link.text === "Services" || item.link.text === "Dienstleistungen") && <TiArrowSortedDown />}
+                  />
+                  {(item.link.text === "Services" ||
+                    item.link.text === "Dienstleistungen") && (
+                    <TiArrowSortedDown />
+                  )}
                 </div>
 
                 {/* Submenu for "Services" */}
-                { (item.link.text === "Services" || item.link.text === "Dienstleistungen") && (
+                {(item.link.text === "Services" ||
+                  item.link.text === "Dienstleistungen") && (
                   <div className="absolute left-0 mt-2 w-64 bg-white shadow-lg rounded-md opacity-0 group-hover:opacity-100 transition-opacity duration-200">
                     {data.data.slices[0]?.primary?.service_sub_menu.map(
-                      (subItem: Simplify<TopNavbarSliceDefaultPrimaryServiceSubMenuItem>, subIndex: number) => {
+                      (
+                        subItem: Simplify<TopNavbarSliceDefaultPrimaryServiceSubMenuItem>,
+                        subIndex: number
+                      ) => {
                         return (
                           <PrismicNextLink
                             key={subIndex}
@@ -66,4 +79,3 @@ export default async function Header({ lang }: { lang: string }) {
     </div>
   );
 }
-
--- src/slices/Vacencies/index.tsx
+++ src/slices/Vacencies/index.tsx
@@ -51,55 +51,66 @@ const Vacencies: FC<VacenciesProps> = ({ slice }) => {
         />
 
         {/* Cards Section */}
-        <div
-          className="grid md:grid-cols-[repeat(2,minmax(300px,350px))] xl:grid-cols-4 justify-center w-full max-w-[1240px] mx-auto  gap-4 place-items-center"
-          data-aos="fade-right"
-          data-aos-delay="50"
-          data-aos-offset="300"
-        >
-          {slice.primary.vacency_card.map((item, index) => (
-            <div
-              key={index}
-              className="rounded-lg pt-1 pb-5 px-2 min-w-[17rem] shadow-lg bg-white text-black flex flex-col justify-center items-center text-center "
-              style={{
-                background:
-                  "radial-gradient(at top center, #235683 0%, #0D2F4B 100%)",
-              }}
-            >
-              {item.card_image && (
-                <div className="mb-4 flex justify-center">
-                  <Image
-                    src={item.card_image.url || ""}
-                    alt="person image"
-                    width={203}
-                    height={203}
-                  />
-                </div>
-              )}
-
-              <PrismicRichText
-                field={item.card_heading}
-                components={{
-                  heading1: ({ children }) => (
-                    <h1 className={`text-2xl font-bold  text-[#6FDCD6] ${montserrat.className}`}>
-                      {children}
-                    </h1>
-                  ),
+        {slice.primary.vacency_card.length > 0 ? (
+          <div
+            className="grid md:grid-cols-[repeat(2,minmax(300px,350px))] xl:grid-cols-4 justify-center w-full max-w-[1240px] mx-auto  gap-4 place-items-center"
+            data-aos="fade-right"
+            data-aos-delay="50"
+            data-aos-offset="300"
+          >
+            {slice.primary.vacency_card.map((item, index) => (
+              <div
+                key={index}
+                className="rounded-lg pt-1 pb-5 px-2 min-w-[17rem] shadow-lg bg-white text-black flex flex-col justify-center items-center text-center "
+                style={{
+                  background:
+                    "radial-gradient(at top center, #235683 0%, #0D2F4B 100%)",
                 }}
-              />
-             <h2 className={`text-2xl font-bold  text-[#6FDCD6] ${montserrat.className}`}>{item.card_sub_heading}</h2>
-             <h3 className={`text-2xl font-bold  text-[#6FDCD6]  ${montserrat.className}`}>{item.card_label}</h3>
-              {item.card_button && (
-                <PrismicNextLink
-                  field={item.card_button}
-                  className="inline-block text-start bg-[#6FDCD6] text-black px-4 py-2 mt-4 rounded-lg font-medium hover:bg-[#5AC1B9]"
-                >
-                  Read More
-                </PrismicNextLink>
-              )}
-            </div>
-          ))}
-        </div>
+              >
+                {item.card_image && (
+                  <div className="mb-4 flex justify-center">
+                    <Image
+                      src={item.card_image.url || ""}
+                      alt="person image"
+                      width={203}
+                      height={203}
+                    />
+                  </div>
+                )}
+
+                <PrismicRichText
+                  field={item.card_heading}
+                  components={{
+                    heading1: ({ children }) => (
+                      <h1 className={`text-2xl font-bold  text-[#6FDCD6] ${montserrat.className}`}>
+                        {children}
+                      </h1>
+                    ),
+                  }}
+                />
+                <h2 className={`text-2xl font-bold  text-[#6FDCD6] ${montserrat.className}`}>{item.card_sub_heading}</h2>
+                <h3 className={`text-2xl font-bold  text-[#6FDCD6]  ${montserrat.className}`}>{item.card_label}</h3>
+                {item.card_button && (
+                  <PrismicNextLink
+                    field={item.card_button}
+                    className="inline-block text-start bg-[#6FDCD6] text-black px-4 py-2 mt-4 rounded-lg font-medium hover:bg-[#5AC1B9]"
+                  >
+                    Read More
+                  </PrismicNextLink>
+                )}
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div
+            className="text-center text-xl text-[#6FDCD6] font-bold"
+            data-aos="fade-up"
+            data-aos-delay="50"
+            data-aos-offset="300"
+          >
+            <p>Currently, we do not have any vacancies. Stay tuned for updates!</p>
+          </div>
+        )}
       </div>
     </Bounded>
   );
