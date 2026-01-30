8debea84ceb54fa24c4497f405c58cb8d70f9586
hash1000 <hashirmughal1000@gmail.com>
2026-01-31 02:56:38 +0500

    changes make new page

--- src/slices/InformationObligations/index.tsx
+++ src/slices/InformationObligations/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
-import { SliceComponentProps } from "@prismicio/react";
+import { PrismicRichText, SliceComponentProps } from "@prismicio/react";
+import Bounded from "@/components/Bounded";
 
 /**
  * Props for `InformationObligations`.
@@ -12,20 +13,75 @@ export type InformationObligationsProps =
  * Component for "InformationObligations" Slices.
  */
 const InformationObligations: FC<InformationObligationsProps> = ({ slice }) => {
+  const nonRepeated = slice.primary.non_repeated;
+  const repeatable = slice.primary.repeatable;
+
   return (
-    <section
+    <Bounded
+      className="py-[100px] mx-auto !w-4/5"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      Placeholder component for information_obligations (variation:{" "}
-      {slice.variation}) slices.
-      <br />
-      <strong>You can edit this slice directly in your code editor.</strong>
-      {/**
-       * 💡 Use the Prismic MCP server with your code editor
-       * 📚 Docs: https://prismic.io/docs/ai#code-with-prismics-mcp-server
-       */}
-    </section>
+      <div className="max-w-7xl mx-auto pt-[150px] pb-[50px]">
+        {/* Non Repeated (one time) */}
+        {Array.isArray(nonRepeated) &&
+
+          nonRepeated.map((item, index) => (
+            <div key={index} className="mb-8">
+              <div className="text-5xl font-bold text-[#5AB7B5] my-4 pb-[50px]">
+                <PrismicRichText field={item.heading} />
+              </div>
+              <div className="mb-10">
+                {/* content should be Rich Text (paragraphs) */}
+                <PrismicRichText
+                  field={item.contant}
+                  components={{
+                    paragraph: ({ children }) => (
+                      <p className="text-lg mb-4">{children}</p>
+                    ),
+                    listItem: ({ children }) => (
+                      <li className="ml-6 list-disc">{children}</li>
+                    ),
+                    oListItem: ({ children }) => (
+                      <li className="ml-6 list-decimal">{children}</li>
+                    ),
+                  }}
+                />
+              </div>
+            </div>))}
+
+        {/* Repeatable (many sections) */}
+        {Array.isArray(repeatable) &&
+          repeatable.map((item, index) => (
+            <div key={index} className="mt-8">
+              <PrismicRichText
+                field={item.heading}
+                components={{
+                  heading2: ({ children }) => (
+                    <h2 className="text-2xl font-semibold mb-2">{children}</h2>
+                  ),
+                  paragraph: ({ children }) => (
+                    <p className="text-2xl font-semibold mb-2">{children}</p>
+                  ),
+                }}
+              />
+
+              <PrismicRichText
+                field={item.body}
+                components={{
+                  paragraph: ({ children }) => <p className="mb-4">{children}</p>,
+                  listItem: ({ children }) => (
+                    <li className="ml-6 list-disc">{children}</li>
+                  ),
+                  oListItem: ({ children }) => (
+                    <li className="ml-6 list-decimal">{children}</li>
+                  ),
+                }}
+              />
+            </div>
+          ))}
+      </div>
+    </Bounded>
   );
 };
 
